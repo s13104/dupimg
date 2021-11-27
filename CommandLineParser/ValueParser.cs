f14c50c86a7592444a2f2eca12612c5351b92743
鈴木久利 <s13104@hotmail.co.jp>
2021-11-27 16:09:33 +0900

    新規作成

---
+++ CommandLineParser/ValueParser.cs
@@ -0,0 +1,60 @@
+    public interface IValueParser<out T>
+    {
+        T Parse(string val);
+    }
+
+    class ValueParser : IValueParser<string>
+    {
+        public virtual string Parse(string val)
+        {
+            return val;
+        }
+    }
+
+    public abstract class ValueParserConstraint<T> where T : struct, IComparable
+    {
+        public T MaxValue { get; private set; }
+        public T MinValue { get; private set; }
+
+        public ValueParserConstraint(T min, T max)
+        {
+            SetValue(min, max);
+        }
+
+        protected T CheckValue(T val)
+        {
+            return val.CompareTo(MinValue) == -1 ? MinValue
+                : val.CompareTo(MaxValue) == 1 ? MaxValue
+                : val;
+        }
+
+        protected void SetValue(T min, T max)
+        {
+            MinValue = min.CompareTo(max) == -1 ? min : max;
+            MaxValue = max.CompareTo(min) == 1 ? max : min;
+        }
+    }
+
+    public class ValueParserDouble : ValueParserConstraint<double>, IValueParser<double>
+    {
+        public ValueParserDouble(double min, double max) : base(min, max)
+        {
+        }
+
+        public virtual double Parse(string val)
+        {
+            return CheckValue(double.TryParse(val, out var tmp) ? tmp : default(double));
+        }
+    }
+
+    public class ValueParserInteger : ValueParserConstraint<int>, IValueParser<int>
+    {
+        public ValueParserInteger(int min, int max) : base(min, max)
+        {
+        }
+
+        public virtual int Parse(string val)
+        {
+            return CheckValue(int.TryParse(val, out var tmp) ? tmp : default(int));
+        }
+    }
---
+++ csvcomp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(args.Count());
+foreach(var arg in args)
+{
+    Console.WriteLine(arg);
+}
